a8a36e4c8c840528af2e588080ba66a0199b30d1
Kirill <43183130+Kvarki-rM@users.noreply.github.com>
2018-12-27 17:13:04 +0300

    Update Files.kt

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -64,7 +64,7 @@ fun howManyTimes(key: String, word: String): Int {
     for (i in min until temp.size) {
         used += temp[i]
         if (key in used) {
-            min +=  (i + 1)
+            min +=  (i - key.length + 1)
             n++
             used = ""
         }
